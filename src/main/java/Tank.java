834793e1b8bd58ff85e1ee5068953e7f5ae26d45
junyi <e1356355@u.nus.edu>
2025-09-01 17:15:53 +0800

    Renamed chatbot from "Duke" to "Tank"
    Implemented an initial skeletal version of it that simply greets the user and exits.

---
+++ src/main/java/Tank.java
@@ -0,0 +1,11 @@
+public class Tank {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Tank");
+        System.out.println("What can I do for you?\n");
+        System.out.println("____________________________________________________________");
+
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
