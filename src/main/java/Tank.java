16a489bc3bdcfd96e755ad2c9e060747a0508d50
junyi <e1356355@u.nus.edu>
2025-09-01 20:39:51 +0800

    Added the ability to store whatever text entered by the user and display them back to the user using the command "list".
    More than 100 texts are able to be stored.

--- src/main/java/Tank.java
+++ src/main/java/Tank.java
@@ -1,25 +1,42 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+
 
 public class Tank {
+    static void displayList(ArrayList<String> list) {
+        for (String text : list) {
+            System.out.println(text);
+        }
+    }
+
     public static void main(String[] args) {
+
         String line = "";
         Scanner in = new Scanner(System.in);
+        ArrayList<String> listOfTexts = new ArrayList<>();
+        int numberInList = 1;
 
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Tank");
-        System.out.println("What can I do for you?\n");
+        System.out.println("What shall we talk about today?\n");
         System.out.println("____________________________________________________________\n");
         do {
             line = in.nextLine();
+            if (line.equals(("list"))) {
+                displayList(listOfTexts);
+                continue;
+            }
+
             if (!line.equals("bye")) {
                 System.out.println("____________________________________________________________");
-                System.out.println("\t" + line);
+                System.out.println("\t" + "added: " + line);
                 System.out.println("____________________________________________________________");
+                listOfTexts.add(Integer.toString(numberInList) + ". " + line);
+                numberInList++;
             }
         } while (!line.equals("bye"));
         System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
-
     }
 }
