ba3190f8afb294a95f91362410a8a4a180eed32a
junyi <e1356355@u.nus.edu>
2025-09-01 21:21:02 +0800

    Renamed list to tasks. Functionality of "list" command is preserved.
    Implemented task tracking using Task class, with ability to mark and unmark tasks as done/not done.
    
    Example input: "mark 1"
    Example input: "unmark 1"
    
    Modified text prompts to sound more friendly.

--- src/main/java/Tank.java
+++ src/main/java/Tank.java
@@ -3,27 +3,62 @@ import java.util.ArrayList;
 
 
 public class Tank {
-    static void displayList(ArrayList<String> list) {
-        for (String text : list) {
-            System.out.println(text);
+    static void displayList(ArrayList<Task> list) {
+        int counter = 1;
+        System.out.println("\tHere are the tasks in your list:");
+        for (Task item : list) {
+            System.out.println("\t" + counter + "." + item.getStatusIcon() + " " + item.description);
+            counter++;
         }
+        System.out.println("____________________________________________________________");
+    }
+
+    static void markTaskDone(ArrayList<Task> list, String line) {
+        String[] parts = line.split(" ");
+        int ArrayIndex = Integer.parseInt(parts[1]) - 1;
+        list.get(ArrayIndex).setDone();
+        System.out.println("____________________________________________________________");
+        System.out.println("\t Good job! I've marked this task done:");
+        System.out.println("\t\t" + list.get(ArrayIndex).getStatusIcon() + " " + list.get(ArrayIndex).description);
+        System.out.println("____________________________________________________________");
+    }
+
+    static void markTaskNotDone(ArrayList<Task> list, String line) {
+        String[] parts = line.split(" ");
+        int ArrayIndex = Integer.parseInt(parts[1]) - 1;
+        list.get(ArrayIndex).setNotDone();
+        System.out.println("____________________________________________________________");
+        System.out.println("\t Alright boss! I've marked this task as not done yet:");
+        System.out.println("\t\t" + list.get(ArrayIndex).getStatusIcon() + " " + list.get(ArrayIndex).description);
+        System.out.println("____________________________________________________________");
     }
 
     public static void main(String[] args) {
 
         String line = "";
         Scanner in = new Scanner(System.in);
-        ArrayList<String> listOfTexts = new ArrayList<>();
-        int numberInList = 1;
+        ArrayList<Task> listOfTasks = new ArrayList<>();
+        int numberOfTasks = 0;
 
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Tank");
         System.out.println("What shall we talk about today?\n");
         System.out.println("____________________________________________________________\n");
+
         do {
             line = in.nextLine();
             if (line.equals(("list"))) {
-                displayList(listOfTexts);
+                displayList(listOfTasks);
+                continue;
+            }
+
+            if (line.contains(("unmark"))) {
+                markTaskNotDone(listOfTasks, line);
+                continue;
+            }
+
+            if (line.contains(("mark"))) {
+                markTaskDone(listOfTasks, line);
                 continue;
             }
 
@@ -31,8 +66,8 @@ public class Tank {
                 System.out.println("____________________________________________________________");
                 System.out.println("\t" + "added: " + line);
                 System.out.println("____________________________________________________________");
-                listOfTexts.add(Integer.toString(numberInList) + ". " + line);
-                numberInList++;
+                listOfTasks.add(new Task(line));
+                numberOfTasks++;
             }
         } while (!line.equals("bye"));
         System.out.println("____________________________________________________________");
