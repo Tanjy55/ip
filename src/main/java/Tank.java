2d9e2980e9e7a8c001c2d172bbc528301c9742ea
junyi <e1356355@u.nus.edu>
2025-09-01 20:18:26 +0800

    Added Echo function: When user inputs keyboard string, Tank echos string back.
    Program terminates when user inputs "bye"

--- src/main/java/Tank.java
+++ src/main/java/Tank.java
@@ -1,11 +1,25 @@
+import java.util.Scanner;
+
 public class Tank {
     public static void main(String[] args) {
+        String line = "";
+        Scanner in = new Scanner(System.in);
+
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Tank");
         System.out.println("What can I do for you?\n");
+        System.out.println("____________________________________________________________\n");
+        do {
+            line = in.nextLine();
+            if (!line.equals("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("\t" + line);
+                System.out.println("____________________________________________________________");
+            }
+        } while (!line.equals("bye"));
         System.out.println("____________________________________________________________");
-
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
+
     }
 }
